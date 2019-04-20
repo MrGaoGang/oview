96e7e814797de6571672afbe140ff4f7d76960df
mrgao <gaogangwork@qq.com>
2019-04-20 09:21:45 +0800

    Update webpack.dev.config.js

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,18 +2,20 @@ var merge = require("webpack-merge");
 var baseConfig = require("./webpack.base.config.js");
 var VueLoaderPlugin = require("vue-loader/lib/plugin");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var HtmlwebpackPlugin = require("html-webpack-plugin");
 var path = require("path");
-var webpack=require("webpack");
+var webpack = require("webpack");
 baseConfig.plugins = [];
 module.exports = merge(baseConfig, {
+  entry: {
+    //为了兼容ie加入入口文件babel-polyfill
+    app: ["babel-polyfill", "./examples/main.js"] //指定入口文件
+  },
   output: {
-    publicPath: '/dist/',
+    publicPath: "/dist/",
     filename: "main.js" //输出文件
   },
-  devtool: '#eval-source-map',//开启测试环境调试
+  devtool: "#eval-source-map", //开启测试环境调试
   plugins: [
-
     new VueLoaderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     //将css文件合并为main.css
