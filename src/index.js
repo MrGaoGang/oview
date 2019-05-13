b8b19231b1fb22c95f28a20f2e6a4d4afa6f26a4
mrgao <gaogangwork@qq.com>
2019-05-13 16:15:24 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
 import Toast from "./components/toast";
-import Chart from "./components/chart/Chart";
-import Pie from "./components/chart/Pie";
-
+import Chart from "./components/chart/Chart.vue";
+import Pie from "./components/chart/Pie.vue";
 
 const oView = {
-    Toast,
-    Chart,
-    Pie
+  Toast,
+  Chart,
+  Pie
 };
 
+const install = function(Vue) {
+  if (install.install) {
+    return;
+  }
 
+  Object.keys(oView).forEach(each => {
+    Vue.component(each, oView[each]);
+  });
 
-const install = function (Vue) {
-    if (install.install) {
-        return;
-    }
+  Vue.prototype.$toast = Toast;
+};
 
-    Object.keys(oView).forEach(each => {
-        Vue.component(each, oView[each])
-    });
-
-    Vue.prototype.$toast = Toast;
-
-    if (typeof window !== 'undefined' && window.Vue) {
-        install(window.Vue);
-    }
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
 }
 
-export default {
-    install,
-    ...oView
+const ALL = {
+  install,
+  ...oView
 };
+
+export { install, Pie, Chart, Toast };
+export default ALL;
