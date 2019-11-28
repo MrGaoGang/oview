87e4c2ecd9cb2fa239b13ad961254b8cc9e4692c
mrgao <gaogangwork@qq.com>
2019-11-28 10:43:25 +0800

    Update utils.js

--- src/components/chart/utils/utils.js
+++ src/components/chart/utils/utils.js
@@ -5,7 +5,7 @@
  */
 export function throttle(func, wait) {
   let timeout = null;
-  return function(...args) {
+  return function (...args) {
     let context = this;
     if (!timeout) {
       setTimeout(() => {
@@ -14,4 +14,18 @@ export function throttle(func, wait) {
       });
     }
   };
+}
+/**
+ * 获取默认宽高
+ * @param {*} data 
+ */
+export function getValue(data) {
+  if (typeof data === "number") {
+    return data + 'px';
+  } else {
+    if (data.indexOf("%") == -1 && data.indexOf("px") == -1) {
+      return data + 'px';
+    }
+    return data;
+  }
 }
